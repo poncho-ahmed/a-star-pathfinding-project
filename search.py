06848a6094e5e4ba64c4eb8331679d8b46cb475a
Ahmed Mohamed <emohamed@memphis.edu>
2024-03-15 22:44:45 -0500

    rename files

--- search.py
+++ search.py
@@ -17,7 +17,7 @@ class Graph:
             self.nodes[neighbor].append(node)
 
 
-def bfs(graph, start, goal=None): # Breath-First Search
+def bfs(graph, start, goal=None):  # Breath-First Search
     visited = set()
     queue = [(start, [start])]
 
@@ -33,6 +33,27 @@ def bfs(graph, start, goal=None): # Breath-First Search
                 if neighbor not in visited:
                     queue.append((neighbor, path + [neighbor]))
     return None  # Return None if goal is not found
+
+
+def dfs(graph, start, goal=None, path=None, visited=None):  # Depth-First Search
+    if visited is None:
+        visited = set()
+    if path is None:
+        path = [start]
+
+    visited.add(start)
+
+    if start == goal:
+        return path
+
+    for neighbor in graph.nodes[start]:
+        if neighbor not in visited:
+            # Recursive call to travel down depths
+            new_path = dfs(graph, neighbor, goal, path=path + [neighbor], visited=visited)
+
+            if new_path:
+                return new_path
+    return None
 
 
 if __name__ == '__main__':
@@ -57,3 +78,6 @@ if __name__ == '__main__':
 
     print("BFS starting from node A:")
     print(bfs(graph, "A", "I"))
+
+    print("DFS starting from node A:")
+    print(dfs(graph, "A", "I"))
